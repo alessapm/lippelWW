8635a0cac843b3ab740ce916b74b61e26ffb6455
Alessa Messineo <apmessineo@gmail.com>
2017-11-24 12:23:18 -0500

    style submit btn. add background images to .pic

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
    $(document).ready(() => {
 
-      $('.parallax').parallax();
+    console.log('js synched');
+
+    const photoArr = ['chuttersnap-176806-compressed.jpg', 'delanoix2-compressed.jpg'];
+
+    $('.parallax').parallax();
+
+
 
     });
 
